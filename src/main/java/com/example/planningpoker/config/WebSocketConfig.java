a2178a18db4ba89ecd17c1c24b9a6f123af2860e
Luca Barden <lubar105@hhu.de>
2025-02-13 00:31:02 +0100

    Origin Fix

--- src/main/java/com/example/planningpoker/config/WebSocketConfig.java
+++ src/main/java/com/example/planningpoker/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // Register the /ws endpoint, enabling SockJS fallback options
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
