5844e5151c3349cd2c915b96bd160db28a588c30
Luca Barden <lubar105@hhu.de>
2025-02-14 01:12:01 +0100

    Improved Playing Animations and Reset Feature

--- src/main/java/com/example/planningpoker/model/PokerMessage.java
+++ src/main/java/com/example/planningpoker/model/PokerMessage.java
@@ -1,7 +1,12 @@
 package com.example.planningpoker.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Collection;
 
+@Setter
+@Getter
 public class PokerMessage {
 
     public enum MessageType {
@@ -13,6 +18,7 @@ public class PokerMessage {
         LEAVE
     }
 
+    // Getters and setters
     private MessageType type;
     private String roomId;
     private String playerId;
@@ -22,50 +28,8 @@ public class PokerMessage {
     // Fields for broadcasting the updated room state:
     private Collection<Player> players;
     private boolean revealed;
+    private boolean reset;
 
     public PokerMessage() { }
 
-    // Getters and setters
-    public MessageType getType() {
-        return type;
-    }
-    public void setType(MessageType type) {
-        this.type = type;
-    }
-    public String getRoomId() {
-        return roomId;
-    }
-    public void setRoomId(String roomId) {
-        this.roomId = roomId;
-    }
-    public String getPlayerId() {
-        return playerId;
-    }
-    public void setPlayerId(String playerId) {
-        this.playerId = playerId;
-    }
-    public String getPlayerName() {
-        return playerName;
-    }
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
-    public String getCard() {
-        return card;
-    }
-    public void setCard(String card) {
-        this.card = card;
-    }
-    public Collection<Player> getPlayers() {
-        return players;
-    }
-    public void setPlayers(Collection<Player> players) {
-        this.players = players;
-    }
-    public boolean isRevealed() {
-        return revealed;
-    }
-    public void setRevealed(boolean revealed) {
-        this.revealed = revealed;
-    }
 }
