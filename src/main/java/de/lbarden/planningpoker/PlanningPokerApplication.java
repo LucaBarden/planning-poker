c6ff36eb8a24997a8ccc80fbc5cd8ab288daae7c
Luca Barden <lubar105@hhu.de>
2025-02-16 13:47:16 +0100

    Enable Scheduling

--- src/main/java/de/lbarden/planningpoker/PlanningPokerApplication.java
+++ src/main/java/de/lbarden/planningpoker/PlanningPokerApplication.java
@@ -2,8 +2,10 @@ package de.lbarden.planningpoker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PlanningPokerApplication {
 
     public static void main(String[] args) {
